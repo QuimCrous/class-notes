145f3be267a7d825e4f4a5f9093bde2b1e877981
Quim Crous <joaquimcrous@gmail.com>
2023-01-16 10:19:34 +0100

    ex8

---
+++ exercises/ex8/index.js
@@ -0,0 +1,18 @@
+let a;
+a = 5;
+console.log(a);
+a+=1;
+console.log(a);
+a = a + 1;
+console.log(a);
+a++;
+console.log(a);
+
+
+let b = 3;
+let c = "3";
+console.log(b==c);
+console.log(b===c);
+console.log(b!==c);
+console.log(b>=7);
+console.log(b<=3);
