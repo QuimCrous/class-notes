44cc42da8fde94b89950889bea2f215c362a6081
Quim Crous <joaquimcrous@gmail.com>
2023-01-20 17:03:16 +0100

    clas ex15

--- labs/week-2/lab-2/src/index.js
+++ labs/week-2/lab-2/src/index.js
@@ -43,12 +43,29 @@ function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
   //... your code goes here
+  const productToRemove = target.parentNode.parentNode;
+  const parent = productToRemove.parentNode;
+  console.log(productToRemove);
+  console.log(parent);
+  parent.removeChild(productToRemove);
+  calculateAll();
 }
 
 // ITERATION 5
 
 function createProduct() {
   //... your code goes here
+  console.log("patata");
+  const text = document.querySelector(".create-product").querySelectorAll("input")[0].value;
+  const quantity = document.querySelector(".create-product").querySelectorAll("input")[1].value;
+  console.log(text);
+  console.log(quantity);
+  const table = document.querySelector("tbody");
+  console.log(table);
+  let productRow = table.insertRow().classList.add("product");
+  let cellOne = productRow.insertCell();
+  //cellOne.classList.add("name");
+  //cellOne.innerHTML = `<span>${text}</span>`
 }
 
 window.addEventListener('load', () => {
@@ -57,3 +74,17 @@ window.addEventListener('load', () => {
 
   //... your code goes here
 });
+
+window.addEventListener("load", () => {
+  const removeProductButtonArray = document.querySelectorAll(".btn-remove");
+  removeProductButtonArray.forEach((button) => {
+    button.addEventListener("click", function (e) {
+      removeProduct(e);
+    });
+  })
+})
+
+window.addEventListener("load", () => {
+  const createButton = document.querySelector("#create");
+  createButton.addEventListener("click", createProduct);
+})
