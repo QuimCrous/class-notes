16927f9c1a1e7f0dd729ae69f2cdb221356a755a
Quim Crous <joaquimcrous@gmail.com>
2023-01-23 12:56:56 +0100

    ex16 complete

---
+++ exercises/ex16/script.js
@@ -0,0 +1,27 @@
+/*
+Ejercicio 1:
+Muestra por consola toda la información de tu pokemon favorito usando la API https://pokeapi.co*/
+
+let blastoiser = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon/9").then((response) => response.json()).then((res) => console.log(res)).catch((err) => console.log(err))
+}
+
+blastoiser()
+
+/*
+Ejercicio 2:
+Permite al usuario introducir un número entre el 0 y el 151, que son los únicos pokemon que merecen la pena. Devuélvele la info del pokemon con la id correspondiente.
+*/
+
+let searchById = () => {
+    let pokemon = document.querySelector("body input").value;
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`).then((res) => res.json().then((res) => {
+        console.log(res);
+        let pokemonName = res.species.name;
+        console.log(pokemonName);
+        let title = document.createElement("p");
+        title.innerText = pokemonName;
+        document.querySelector("body").appendChild(title);
+    }).catch((error) => console.log(error)))
+}
+
